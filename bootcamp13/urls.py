09f3d631a42073a05255fdbdd9abf38635c1b3ac
Pauline Korukundo <pktpaulie@gmail.com>
2023-06-22 17:26:38 +0300

    ADD RENDER CONFIGS

---
+++ bootcamp13/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.home, name = 'home'),
+    path('all_students/', views.all_students),  
+    path('update_student/<int:pk>/', views.UpdateStudent),
+    path('delete_student/<int:pk>/', views.DeleteStudent),
+    path('new_student/', views.new_student),
+]
---
+++ bootcamp13/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from .models import *
+
+# 
+from .serializer import StudentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
+def home(request):
+    members = BootcampMembers.objects.all()
+    context = {
+        'members':members
+    }
+    return render(request, 'home.html', context)
+
+
+#  Was proposing we change it function to return all students like this
+
+# The @api_view decorator shows a view function only accessed via HTTP GET requests.
+@api_view(['GET']) 
+# This function takes a HTTP client request parameter.
+def all_students(request):
+    members = BootcampMembers.objects.all()
+    serializer = StudentSerializer(members, many = True)
+    # this helps return the data back to the UI to see whats available in db
+    return Response(serializer.data)
+
+
+# Update student function using pk constratint
+@api_view(['PUT'])
+def UpdateStudent(request, pk):
+    # This method retrieves a single instance of the student model with the given primary key pk.
+    student = BootcampMembers.objects.get(id=pk)
+    serializer = StudentSerializer(instance=student, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response("Student Updated Successfully !!!")
+
+
+# delete student rest funstion basing on pk
+@api_view(['DELETE'])
+def DeleteStudent(request, pk):
+    deletestudent = BootcampMembers.objects.get(id=pk)
+    deletestudent.delete()
+    return Response("Student deleted successfuly !!")
+
+#  RESTAPI Function for creating new student
+@api_view(['POST'])
+def new_student(request):
+    # serializer variable gets data from StudentSerializer class and the pass data from the UI
+    serializer = StudentSerializer(data=request.data)
+    # Checking if the data sent is valid be4 saving the data
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
